2abf9522efabd07487be91249a296bda8635aa49
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-06-08 00:06:40 +0200

    improved numba stubs

--- typings/numba/__init__.pyi
+++ typings/numba/__init__.pyi
@@ -1,18 +1,37 @@
 from collections.abc import Callable
-from typing import Any
+from typing import Any, Literal, ParamSpec, TypeVar
 
+P = ParamSpec("P")
+R = TypeVar("R")
 
-def njit(*args: Any, **kwargs: Any) -> Callable[..., Any]: ...
+def jit(
+    signature_or_function: Callable[P, R] | str | list[str] | None = None,
+    locals: dict[str, Any] = {},
+    cache: bool = False,
+    pipeline_class: type | None = None,
+    boundscheck: bool | None = None,
+    nopython: bool = True,
+    forceobj: bool = False,
+    looplift: bool = True,
+    error_model: Literal["python", "numpy"] = "python",
+    inline: Literal["never", "always"] | Callable[..., bool] = "never",
+    parallel: bool = False,
+    nogil: bool = False,
+) -> Callable[P, R]: ...
 
-class Integer:
-    ...
+class Type:
+    def __call__(self, *args: Any, **kwds: Any) -> Any: ...
 
-class Float:
-    ...
+class Array(Type):
+    def __init__(self, dtype: Any, ndim: Any, layout: Any) -> None: ...
+
+class Integer(Type): ...
+
+class Float(Type):
+    def __getitem__(self, args: Any) -> Array: ...
 
 class prange(object):
-    def __new__(cls, *args: Any) -> range: 
-        ...
+    def __new__(cls, *args: Any) -> range: ...
 
 float32 = Float()
 float64 = Float()
--- typings/numba/experimental/__init__.pyi
+++ typings/numba/experimental/__init__.pyi
@@ -1,4 +1,19 @@
-from collections.abc import Iterable
-from typing import Any
+from collections.abc import Iterable, Callable
+from typing import Any, overload
 
-def jitclass(cls_or_spec: Any|None = None, spec: Iterable[Any] | None = None) -> Any: ...
+
+@overload
+def jitclass[T: type](
+    cls_or_spec: T,
+    spec: Iterable[tuple[str, Any]] | dict[str, Any]
+) -> T: 
+    """Function to JIT-compile a class with a specified type signature."""
+    ...
+
+@overload
+def jitclass[T: type](
+    cls_or_spec: Iterable[tuple[str, Any]] | dict[str, Any] | None = None,
+    spec: Iterable[tuple[str, Any]] | dict[str, Any] | None = None
+) -> Callable[[T], T]: 
+    """Decorator to JIT-compile a class with a specified type signature."""
+    ...
