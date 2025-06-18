f1d79af2553fc5cd6e73a0777a8cf835a345a788
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2025-06-18 15:08:09 +0200

    simplfifying map class to prepare expr refactor

--- src/main.py
+++ src/main.py
@@ -6,24 +6,16 @@ import numpy as np
 import polars as pl
 from numpy.typing import NDArray
 from src.arrays import Array
-
+from src.expressions import Expr
 
 @dataclass(slots=True, repr=False)
 class Map:
     data: dict[str, Array]
-    index: pl.Series
-    names: pl.Series
 
-    def __repr__(self) -> str:
-        return f"Map(data={self.data}\n, index={self.index}\n, names={self.names})"
-
-    def select(self, *arr: str) -> Self:
-        return self.__class__(
-            data={key: self.data[key] for key in arr if key in self.data},
-            index=self.index,
-            names=self.names,
-        )
-
+    def select(self, *exprs: Expr) -> Self:
+        ...
+    def with_columns(self, *exprs: Expr) -> Self:
+        ...
 
 def read_parquet(
     file: Path, names_col: str, index_col: str, values_cols: list[str]
@@ -42,12 +34,8 @@ def read_parquet(
     last_df: pl.DataFrame = df.pivot(
         on=names_col, index=index_col, values=values_cols[-1]
     )
-    index: pl.Series = last_df.get_column(name=index_col)
-    names = pl.Series(
-        name="names", values=[col for col in last_df.columns if col != index_col]
-    )
     data[values_cols[-1]] = Array(
         data=last_df.drop(index_col).to_numpy().astype(dtype=np.float32),
         exprs=[],
     )
-    return Map(data=data, index=index, names=names)
+    return Map(data=data)
